7f8cc8b4c34dc885dd516913d8e7d48ea378ad7d
Matthew Radcliffe <mradcliffe@softpixel.com>
2017-03-24 11:44:31 -0400

    Add assertions to the dump test

--- tests/src/Functional/Command/DatabaseDumpCommandTest.php
+++ tests/src/Functional/Command/DatabaseDumpCommandTest.php
@@ -37,17 +37,33 @@ class DatabaseDumpCommandTest extends TestCase
     }
 
     /**
+     * Get the file name of the backup file.
+     *
+     * @return string
+     *   The file name of the backup file.
+     */
+    protected function getFilename()
+    {
+       return $this->gzip ? self::$result_file . '.gz' : self::$result_file;
+    }
+
+    /**
      * Asserts that the database dump command works.
      *
      * @todo abstract out driver to test PostgreSQL as well.
      */
     public function testExecute()
     {
+        $this->gzip = false;
+        $filename = $this->getFilename();
+
         $input = new ArrayInput(['db:dump', '--result-file=backup.sql']);
         $output = new StreamOutput(fopen('php://memory', 'w', false));
         $kernel = $this->getConsoleKernel();
         $status = $kernel->handle($input, $output);
         $kernel->terminate($input, $status);
+
+        $this->assertFileExists($filename);
     }
 
     /**
@@ -55,11 +71,16 @@ class DatabaseDumpCommandTest extends TestCase
      */
     public function testExecuteWithGzip()
     {
+        $this->gzip = true;
+        $filename = $this->getFilename();
+
         $input = new ArrayInput(['db:dump', '--gzip', '--result-file=backup.sql']);
         $output = new StreamOutput(fopen('php://memory', 'w', false));
         $kernel = $this->getConsoleKernel();
         $status = $kernel->handle($input, $output);
         $kernel->terminate($input, $status);
+
+        $this->assertFileExists($filename);
     }
 
     /**
